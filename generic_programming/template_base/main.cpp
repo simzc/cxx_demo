c36eb061abc59d5b82757896b689e3fbe79cf600
Zhang Chao <60860967+simzc@users.noreply.github.com>
2024-03-06 21:53:40 +0800

    ✨ feat(generic programming ): basic
    1. added new function with cxx17 and cxx20 standard.
    
    joe

--- generic_programming/template_base/main.cpp
+++ generic_programming/template_base/main.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <type_traits>
 #include <iostream>
 
 /// @brief Template function
@@ -22,6 +24,26 @@ struct StructAbs {
 template <auto type>
 constexpr auto const_v = type;
 
+#if 0
+/// @brief Multi-parameter template
+template<typename T1, typename T2, typename T3>
+T3 foo(const T1& x, const T2& y) {
+    return x + y;
+}
+
+/// @brief Multi-parameter template (C++ 17)
+template<typename T1, typename T2>
+auto foo(const T1& x, const T2& y) -> decltype(x+y) {
+    return x + y;
+}
+
+#endif // end if macro
+
+/// @brief Multi-parameter template (C++ 20 with Concept)
+auto foo(const auto& x, const auto& y) {
+    return x + y;
+}
+
 int main(int argc, const char** argv) {
     // Template function
     {
@@ -44,6 +66,13 @@ int main(int argc, const char** argv) {
         std::cout << v1 << std::endl;
         std::cout << v2 << std::endl;
         std::cout << v3 << std::endl;
+    }
+
+    // Multi-parameter template
+    {
+        std::string a = "hello";
+        std::string b = " world!";
+        std::cout << foo(a, b) << std::endl;
     }
 
     return 0;
