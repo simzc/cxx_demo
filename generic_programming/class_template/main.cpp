c029b4f9dee7f3ac2679adf71eebe5a6d1d30786
Zhang Chao <60860967+simzc@users.noreply.github.com>
2024-03-07 21:55:41 +0800

    ✨ feat(generic programming): class template
    1. added new demo about cxx class template.
    
    joe

--- generic_programming/class_template/main.cpp
+++ generic_programming/class_template/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
+template <typename Type, size_t N>
+class Array {
+  public:
+    /// @brief Get the value by index
+    /// @param num Index
+    /// @return Value
+    Type& operator[](size_t num) {
+        if (num < N) {
+            return m_val[num];
+        }
+        throw "Out of the range.";
+    }
+
+    /// @brief Get the size of array
+    size_t size() const { return N; }
+
+  private:
+    Type m_val[N];
+};
+
 int main(int argc, const char** argv) {
-    #if __cplusplus >= 201703L
-        
-    std::cout << "cxx_std is: " << __cplusplus << std::endl;   
-    #endif // end if __cplusplus 
+    Array<int, 10> array;
+    for (size_t i = 0; i < array.size(); i++) {
+        array[i] = i;
+    }
+
+    std::cout << "The size of array is: " << array.size() << std::endl;
+
+    for (size_t i = 0; i < array.size(); i++) {
+        std::cout << "array[" << i << "] is: " << array[i] << std::endl;
+    }
+
     return 0;
 }
