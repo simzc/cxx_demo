36e083665e8b2b1c4178b5b7f708950d45837c0b
simzc <60860967+simzc@users.noreply.github.com>
2024-03-09 15:40:09 +0800

    ✨ feat(generic programming): unique pointer
    1. added new function in class UniquePtr.
    
    joe

--- generic_programming/unique_ptr/main.cpp
+++ generic_programming/unique_ptr/main.cpp
@@ -1,4 +1,3 @@
-
 #include <utility>
 #include <iostream>
 
@@ -6,38 +5,57 @@ class Test {
   public:
     Test() { std::cout << "Test() constructed." << std::endl; }
     ~Test() { std::cout << "Test() destructed." << std::endl; }
+
+    void Show() const { std::cout << "Hello World!" << std::endl; }
 };
 
+/// @brief Unique pointer manager for Type of object
+/// @tparam Type Type of object
 template <typename Type>
 class UniquePtr {
   public:
-    UniquePtr() : m_ptr(nullptr) {
-        std::cout << "Default constructor invoked." << std::endl;
-    }
+    /// @brief Default constructor
+    UniquePtr() : m_ptr(nullptr) { std::cout << "Default constructor invoked." << std::endl; }
 
-    UniquePtr(Type* ptr) : m_ptr(ptr) {
-        std::cout << "Constructor invoked." << std::endl;
-    }
+    /// @brief Constructor with initial pointer
+    UniquePtr(Type* ptr) : m_ptr(ptr) { std::cout << "Constructor invoked." << std::endl; }
 
+    /// @brief Deleted copy constructor
     UniquePtr(const UniquePtr&) = delete;
+
+    /// @brief Deleted assignment operator
     UniquePtr& operator=(const UniquePtr&) = delete;
 
-    UniquePtr(UniquePtr&& rhs) : m_ptr(std::exchange(rhs.m_ptr, nullptr)) {
+    /// @brief Move constructor
+    /// @param rhs Rvalue of object
+    UniquePtr(UniquePtr&& rhs) noexcept : m_ptr(std::exchange(rhs.m_ptr, nullptr)) {
         std::cout << "Move constructor invoked." << std::endl;
     }
 
-    UniquePtr& operator=(UniquePtr&& rhs) {
+    /// @brief Move assignment constructor
+    /// @param rhs Rvalue of object
+    UniquePtr& operator=(UniquePtr&& rhs) noexcept {
         std::cout << "Move assignment operator invoked." << std::endl;
         std::swap(m_ptr, rhs.m_ptr);
         return *this;
     }
 
+    /// @brief Destructor
     ~UniquePtr() {
         if (m_ptr) {
             delete m_ptr;
             m_ptr = nullptr;
         }
     }
+
+    /// @brief Get the pointer of object
+    Type*& Get() { return m_ptr; }
+
+    /// @brief Reload operator of ->
+    Type*& operator->() { return m_ptr; }
+
+    /// @brief Reload operator of *
+    Type& operator*() { return *m_ptr; }
 
   private:
     Type* m_ptr;
@@ -47,6 +65,10 @@ int main(int argc, const char** argv) {
     UniquePtr<Test> test_1 = new Test();
     UniquePtr<Test> test_2 = std::move(test_1);
     UniquePtr<Test> test_3 = static_cast<UniquePtr<Test>&&>(test_2);
+
+    test_3->Show();
+    test_3.Get()->Show();
+    (*test_3).Show();
 
     std::cout << "Unique pointer ending." << std::endl;
 
